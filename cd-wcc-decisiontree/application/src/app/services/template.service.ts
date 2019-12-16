dc2f921b8edc688b64068f3ca922e4d9a214a434
Vincent <vincent.chen02@sap.com>
2019-12-16 18:21:56 +0800

    add get template data function for template service,

--- cd-wcc-decisiontree/application/src/app/services/template.service.ts
+++ cd-wcc-decisiontree/application/src/app/services/template.service.ts
@@ -25,4 +25,32 @@ export class TemplateService {
             });
         decision.hasChildren = !!decision.children.length;
     }
+
+    public static getTemplateData(categoryId: number, templateId: number): any {
+        const template = templates.find(data => data.id === categoryId);
+        const templateItem = template.templates.find(data => data.id === templateId);
+        return TemplateService.formatData(templateItem.values);
+    }
+
+    private static formatData(values: Array<any>) {
+        const result = [];
+        values.forEach(line => {
+            let value = line.value;
+            switch (line.type) {
+                case 'string':
+                    value = line.value + '';
+                    break;
+                case 'number':
+                    value = parseInt(line.value);
+                    break;
+                case 'boolean':
+                    value = !!line.value;
+                    break;
+            }
+            result.push({
+                name: line.key,
+                value: value
+            });
+        });
+    }
 }
